5f6f64527c8b0e8b25add25b0d5290233dfc82cb
unknown <mr.iraq1994@gmail.com>
2022-04-12 00:57:58 +0300

    =second commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <v-container>
       <v-row justify="center">
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="3">
           <v-card max-width="400" class="ma-auto" to="/dashboard/products">
             <v-card-text class="text-center">
               <v-icon style="font-size: 100px" color="error" x-large>mdi-lock</v-icon>
@@ -13,13 +13,35 @@
             </v-card-text>
           </v-card>
         </v-col>
-        <v-col cols="12" md="6">
+        <v-col cols="12" md="3">
           <v-card max-width="400" class="ma-auto" to="/client/products">
             <v-card-text class="text-center">
               <v-icon style="font-size: 100px" x-large color="primary">mdi-account</v-icon>
               <v-spacer/>
               <v-btn color="primary" dark>
                 Client
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-card max-width="400" class="ma-auto" href="https://github.com/hassanbaghdad/gateawy_frontend.git">
+            <v-card-text class="text-center">
+              <v-icon style="font-size: 100px" x-large color="primary">mdi-github</v-icon>
+              <v-spacer/>
+              <v-btn color="primary" dark>
+                 Source Code Frontend Vue.JS
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-card max-width="400" class="ma-auto" href="https://github.com/hassanbaghdad/gateawy_backend.git">
+            <v-card-text class="text-center">
+              <v-icon style="font-size: 100px" x-large color="primary">mdi-github</v-icon>
+              <v-spacer/>
+              <v-btn color="primary" dark>
+                 Source Code Backend Laravel 9
               </v-btn>
             </v-card-text>
           </v-card>
